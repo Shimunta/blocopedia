c68871bbf714a4967b475ddbe91cdf10c0b64e9d
Daniel Lisa <danlisa89@gmail.com>
2016-05-24 18:08:44 -0400

    wiki model implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
+  resources :wikis
+
   devise_for :users
   resources :users, only: [:show]
   get 'welcome/index'
 
   get 'welcome/show'
 
-  root 'users#show'
+  root 'welcome#index'
 end
